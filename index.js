fb462c7c6805e6f3fa3ab1dbf511055172d8d107
arjorb <arjorb@gmail.com>
2022-03-13 13:52:11 +0200

    fontSize customization using javascript

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const messageSearchbox = document.querySelector('#message-search')
 //THEME
 const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span')
+let root = document.querySelector(':root')
+
 
 //remove class function
 
@@ -74,3 +77,44 @@ const closeModal = (e) =>{
 
 themeModal.addEventListener('click',closeModal)
 theme.addEventListener('click',openModal)
+
+// ======================= FONTS ===========================
+
+const removeSizeSelector = () =>{
+    fontSizes.forEach(size =>{
+        size.classList.remove('active');
+    })
+}
+
+
+fontSizes.forEach(size =>{
+    size.addEventListener('click', ()=>{
+    let fontSize;
+    removeSizeSelector();
+    size.classList.add('active');
+
+        if(size.classList.contains('font-size-1')){
+            fontSize = '10px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '5.4rem');
+        }else if(size.classList.contains('font-size-2')){
+            fontSize = '13px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '-7rem');
+        }else if(size.classList.contains('font-size-3')){
+            fontSize = '16px';
+            root.style.setProperty('----sticky-top-left', '-2rem');
+            root.style.setProperty('----sticky-top-right', '-17rem');
+        }else if(size.classList.contains('font-size-4')){
+            fontSize = '19px';
+            root.style.setProperty('----sticky-top-left', '-5rem');
+            root.style.setProperty('----sticky-top-right', '-25rem');
+        }else if(size.classList.contains('font-size-5')){
+            fontSize = '22px';
+            root.style.setProperty('----sticky-top-left', '-12rem');
+            root.style.setProperty('----sticky-top-right', '-35rem');
+        }
+         //Change the fontSize
+    document.querySelector('html').style.fontSize = fontSize;
+    })
+})
